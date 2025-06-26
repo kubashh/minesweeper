24f15545490999bbd08430532b661b5e98b82001
Jakub <jakubhanula0@gmail.com>
2025-06-26 18:10:06 +0200

    2.0.0.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/minesweeper/favicon.png" />
+      </head>
       <body className="w-screen h-screen justify-center content-center bg-zinc-950 text-zinc-900 select-none">
         {children}
       </body>
